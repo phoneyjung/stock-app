cf988cbe236f7314cdb61551a4522728991f7fe2
phoneyjung <phoneyjung@gmail.com>
2026-08-27 00:16:12 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'plp-stock-v13';
+const CACHE = 'plp-stock-v15';
 const CORE = ['./', './index.html', './manifest.json', './firebase-config.js', './icon-192.png'];
 
 self.addEventListener('install', e => {
