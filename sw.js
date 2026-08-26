cd809b618d41d2b5bf6cd5e14f22978d263f308f
phoneyjung <phoneyjung@gmail.com>
2026-08-26 13:10:58 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'plp-stock-v6';
+const CACHE = 'plp-stock-v8';
 const CORE = ['./', './index.html', './manifest.json', './icon-192.png', './bg-pattern.png', './hdr-banner.jpg', './empty-box.jpg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting()));
